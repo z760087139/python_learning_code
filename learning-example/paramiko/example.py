824cf754275ec92a036fd89aeeacfd5c1798b28e
z760087139 <z760087139@gmail.com>
2018-09-27 09:17:24 +0800

    a

--- learning-example/paramiko/example.py
+++ learning-example/paramiko/example.py
@@ -1,5 +1,5 @@
 # paramiko 1.7+
-# get connect using user/password
+# get connect using user/password SSH
 import paramiko
 
 host = '192.168.23.130'
@@ -19,11 +19,36 @@ ssh.connect(hostname=host, username=user, password = password)
 stdin, stdout, stderr = ssh.exec_command('free -m')
 # print result
 print stdout.read()
-# more command
-stdin.write('pwd')
-stdin.flush()
-stdout.read()
+# if more input is needed
+# such as "su - root"
+# stdin,stdout,stderr = ssh.exec_command('su - root')
+# stdin.write('password')
+# stdin.flush()
+# stdout.read()
+
 # close connect
 ssh.close()
 
 
+# using transport
+trans = paramiko.Transport(('192.168.23.130',22))
+trans.connect(username='root',password='zeng123+')
+ssh = paramiko.SSHClient()
+ssh._transport = trans
+stdin.stdout,stderr = ssh.exec_command('free -m')
+print(stdout.read().decode())
+# open channel
+session = trans.open_session()
+session.invoke_shell()
+session.recv()
+session.send('ls -l /tmp')
+# close()
+# trans.close()
+
+# get_pty()
+trans = paramiko.Transport(('192.168.2.129',22))
+trans.start_client()
+trans.auth_password(username='root',password='zeng123+')
+channel.trans.open_session()
+channel.get_pty()
+channel.invoke_shell()
